972eaeed5b05ebd8a2041699e9edac22bc56dac7
Lazaro Menezes <lazarodm@gmail.com>
2024-08-02 18:33:44 -0300

    Add test api

---
+++ api2/Fatec.Rent.API/Fatec.Rent.API/Controllers/VehicleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fatec.Rent.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        private readonly ILogger<VehicleController> logger;
+        private readonly IVehicleRepository repository;
+
+        public VehicleController(ILogger<VehicleController> logger,
+            IVehicleRepository repository)
+        {
+            this.logger = logger;
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Vehicle>> Get()
+        {
+            logger.LogInformation("Get executado com sucesso");
+
+            return await repository.GetAll();
+        }
+    }
+}
---
+++ api2/Fatec.Rent.API/Fatec.Rent.API/DapperDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Fatec.Rent.API
+{
+    public class DapperDbContext : IDbContext
+    {
+        private readonly string? connectionString;
+
+        public DapperDbContext(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("rentDb");
+        }
+
+        public IDbConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
